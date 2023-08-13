df94f74efe38ebdfea23bc999c523e13f68c7ead
Anand4405 <anandpatil4405@gmail.com>
2023-08-13 19:04:03 +0530

    Updated some codes

--- sorting/hoare_partition.cpp
+++ sorting/hoare_partition.cpp
@@ -1,29 +1,34 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int hoare_partition(int arr[],int l,int h){
-int pivot = arr[l];
-int i=l-1,j=h+1;
-    while(true){
-        do{
+int hoare_partition(int arr[], int l, int h)
+{
+    int pivot = arr[l];
+    int i = l - 1, j = h + 1;
+    while (true)
+    {
+        do
+        {
             i++;
-        }while(arr[i]<pivot);
-        do{
+        } while (arr[i] < pivot);
+        do
+        {
             j--;
-        }while(arr[j]>pivot);
-        if(i>=j)
-        return j;
-        swap(arr[i],arr[j]);
+        } while (arr[j] > pivot);
+        if (i >= j)
+            return j;
+        swap(arr[i], arr[j]);
     }
-
 }
-int main(){
-int arr[] = {7,3,5,9,4,6};
-hoare_partition(arr,0,5);
-for(int x:arr){
-    cout<<x<<" ";
-}
+int main()
+{
+    int arr[] = {7, 3, 5, 9, 4, 6};
+    hoare_partition(arr, 0, 5);
+    for (int x : arr)
+    {
+        cout << x << " ";
+    }
 
     return 0;
 }
